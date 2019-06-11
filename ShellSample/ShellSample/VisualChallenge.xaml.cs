22474687062191a45c0fe77d85183f6fb8ae3843
Abhijeet <abhijeet.99@hotmail.com>
2019-06-11 13:13:25 +0530

    Interim commit

---
+++ ShellSample/ShellSample/VisualChallenge.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellSample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VisualChallenge : ContentPage
+	{
+		public VisualChallenge ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Device.OpenUri(new Uri("https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/visual"));
+
+            Shell.Current.GoToAsync("//Contacts2");
+        }
+    }
+}
